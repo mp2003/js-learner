2fc698a64db20a6b6cf7612e74f6830bda6f57ff
mp2003 <m.p6092003@gmail.com>
2023-08-01 17:15:28 +0530

    day3 of js learning

---
+++ 14.js
@@ -0,0 +1,13 @@
+const user = [
+  { userId: 1, username: "harshit" },
+  { userId: 2, username: "mohit" },
+  { userId: 3, username: "kamal" },
+  { userId: 4, username: "hemal" },
+  { userId: 5, username: "milind" },
+];
+
+//   const [ user1 , user2 , user3 , user4]=user;
+//   console.log(user1,user2,user3,user4);
+
+const [{ userId }, , , , { username }] = user;
+console.log(userId, username);
