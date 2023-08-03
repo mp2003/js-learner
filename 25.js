e81cbf571e2f47b22ad6ffe21b834957599a77c0
mp2003 <m.p6092003@gmail.com>
2023-08-03 23:45:57 +0530

    learning js day5

--- 25.js
+++ 25.js
@@ -12,3 +12,17 @@ person2.set(1, "one");
 
 console.log(person);
 console.log(person2);
+
+console.log(person2.get("name"));
+console.log(person2.keys());
+
+for (let keys of person2) {
+  console.log(keys);
+}
+
+const person3 = new Map([
+  ["name", "jhon"],
+  ["age", 35],
+  ["one", 1],
+]);
+console.log(person3);
