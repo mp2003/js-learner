402e303c25663d334b9c8ddf675236d8e022b6ec
mp2003 <m.p6092003@gmail.com>
2023-08-03 23:39:41 +0530

    learning js day5

--- 25.js
+++ 25.js
@@ -1,1 +1,14 @@
- 
+const person = {
+  name: "John",
+  age: 34,
+  1: "one",
+};
+// object literal
+
+const person2 = new Map();
+person2.set("name", "John");
+person2.set("age", 34);
+person2.set(1, "one");
+
+console.log(person);
+console.log(person2);
