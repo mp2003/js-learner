4ea424ab498c37ae185f03e57e0c56bb7f9a8834
mahi5256 <m.p6092003@gmail.com>
2023-08-10 22:12:52 +0530

    k

--- 31.js
+++ 31.js
@@ -11,7 +11,7 @@ const userMethod = {
     email: "abc@example.com",
     about: userMethod.aboutRequest,
   };
-  const user1=[``];
+  const user1=[`military`];
   const user2 ={
     name:"mno",
     age: 6,
