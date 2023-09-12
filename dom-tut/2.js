0ec6acec2eaba55bdbfe54bffeda0d6351380c02
mahi5256 <m.p6092003@gmail.com>
2023-09-13 02:17:02 +0530

    day2

--- dom-tut/2.js
+++ dom-tut/2.js
@@ -1,5 +1,5 @@
-// const rootNode = document.getRootNode();
-// console.log(rootNode);
+const rootNode = document.getRootNode();
+console.log(rootNode);
 let childNode = rootNode.childNodes[0];
 console.log(childNode.childNodes);
 const head = childNode.childNodes[0];
