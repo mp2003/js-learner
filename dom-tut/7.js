9ca73c947afa77c406a36c62dd76d1c220117a9b
mahi5256 <m.p6092003@gmail.com>
2023-09-19 22:56:49 +0530

    removing depnedences

--- dom-tut/7.js
+++ dom-tut/7.js
@@ -1,13 +1,16 @@
 const btn = document.querySelector('button');
 const body = document.body;
+const h1 = document.querySelector('h1');
 const randomColorGenerator =()=>{
     const red = Math.floor(Math.random()*255);
     const green = Math.floor(Math.random()*255);
     const blue = Math.floor(Math.random()*255);
     const color= `rgba(${red},${green},${blue})`;
-    console.log(color);
+    return color;
 }
 
 btn.addEventListener('click',() => {
     const randomColor = randomColorGenerator();
+    body.style.backgroundColor = randomColor;
+    h1.textContent = `current color: ${randomColor}`;
 });
