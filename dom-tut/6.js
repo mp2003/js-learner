71aa812ba282e7821e1496f16239a304449f2f55
mahi5256 <m.p6092003@gmail.com>
2023-09-19 20:58:43 +0530

    removing depnedences

--- dom-tut/6.js
+++ dom-tut/6.js
@@ -1,2 +1,10 @@
-const allButtons = document.querySelectorAll('.my-button button');
-console.dir(allButtons);
+const allButtons = document.querySelectorAll(".my-button button");
+// console.dir(allButtons);
+for (button of allButtons) {
+  button.addEventListener("mouseover", function (e) {
+    e.currentTarget.style.backgroundColor ='yellow';
+  });
+  button.addEventListener("mouseout", function (e) {
+    e.currentTarget.style.backgroundColor ='white';
+  });
+}
