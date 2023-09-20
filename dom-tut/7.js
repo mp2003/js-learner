a27197a556f5ed17f0ee57d96b4b54a55bfa7ac2
mahi5256 <m.p6092003@gmail.com>
2023-09-20 17:36:41 +0530

    js update

--- dom-tut/7.js
+++ dom-tut/7.js
@@ -1,3 +1,17 @@
-const button = document.querySelector('button');
+const button = document.querySelector("button");
 // console.dir(button);
-
+const body = document.body;
+// console.log(body);
+const randomColor = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const color = `rgb("${red}", ${green}, ${blue})`;
+  return color;
+};
+button.addEventListener("click", () => {
+    const color = randomColor();
+    body.style.backgroundColor = color;
+    // console.log(color);
+    // body.textContent = `current color: ${randomColor()}`;
+})
