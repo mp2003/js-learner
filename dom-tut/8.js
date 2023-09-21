cd44014a2b2158db4db6c236afc4e5d7e629b48a
mahi5256 <m.p6092003@gmail.com>
2023-09-21 17:07:13 +0530

    js update

--- dom-tut/8.js
+++ dom-tut/8.js
@@ -1,9 +1,17 @@
 const todoForm = document.querySelector(".todo-form");
 const todoInput = document.querySelector('.todo-form input[type="text"]');
+const ul = document.querySelector(".todo-list");
 const todoSubmit = document.querySelector('.todo-form input[type="submit"]');
 // console.log(todoInput.value);
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
   // console.log('submitted');
+  const newLi = document.createElement("li");
+  newLi.innerHTML = `<span class="text">${todoInput.value}</span>
+  <div class="todo-container">
+    <button class="todo-btn remove">Remove</button>
+    <button class="todo-btn done">Done</button>
+    </div>`;
+  ul.append(newLi);
   todoInput.value = "";
 });
