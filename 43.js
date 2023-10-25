c8f1b1e45bdbb94b22d85a5322266ba903ae7a5f
mahi5256 <m.p6092003@gmail.com>
2023-10-25 20:03:06 +0530

    async prog

--- 43.js
+++ 43.js
@@ -1,9 +1,10 @@
 console.log("script started");
-setTimeout(() => {
+const id = setTimeout(() => {
     console.log("hello");
 }, 0);
 for (let index = 0; index < 100; index++) {
     console.log("... " + index);
 
 }
+clearTimeout(id);
 console.log("script finished");
