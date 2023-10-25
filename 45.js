7307de86856c608f21d113ae03efb432d175dea5
mahi5256 <m.p6092003@gmail.com>
2023-10-25 20:59:45 +0530

    setinterval

--- 45.js
+++ 45.js
@@ -1,7 +1,13 @@
 const body = document.body;
-setInterval(() => {
+const id = setInterval(() => {
     const red = Math.round(Math.random() * 255);
     const green = Math.round(Math.random() * 255);
     const blue = Math.round(Math.random() * 255);
     body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }, 1000);
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('clicked');
+    clearInterval(id)
+});
