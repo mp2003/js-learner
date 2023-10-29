fc27b2fa0dbea34bafa3bc02ec5fbce8ded7160f
mahi5256 <m.p6092003@gmail.com>
2023-10-29 14:51:19 +0530

    json

--- 52.js
+++ 52.js
@@ -1,21 +1,25 @@
-console.log('srcipt started');
-const URL = 'https://jsonplaceholder.typicode.com/posts'
+console.log("srcipt started");
+const URL = "https://jsonplaceholder.typicode.com/posts";
 function myPromise(method, URL) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.open(method, URL, true);
-        console.log(xhr.readyState);
+        // console.log(xhr.readyState);
         xhr.onreadystatechange = () => {
             if (xhr.readyState === 4) {
-                console.log(xhr.readyState);
+                // console.log(xhr.readyState);
                 resolve(xhr.response);
             }
         };
         xhr.send();
-
-    })
-};
-myPromise('GET', URL)
+    });
+}
+myPromise("GET", URL)
     .then((response) => {
         return JSON.parse(response);
-    }).then((data) => console.log(data))
+    })
+    .then((data) => {
+        const id = data[4].id;
+        const URL2 = `${URL}/${id}`;
+        console.log(URL2);
+    });
