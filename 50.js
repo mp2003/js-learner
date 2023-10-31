4db2efe17069dcea8e54b956b38dd6cdffef82b8
mahi5256 <m.p6092003@gmail.com>
2023-10-31 18:56:07 +0530

    xhr

--- 50.js
+++ 50.js
@@ -1,22 +1,23 @@
-const URL = 'https://jsonplaceholder.typicode.com/posts';
+console.log("xhr ajax request");
+
+const URL = "https://jsonplaceholder.typicode.com/posts";
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET', URL, true);
+xhr.open("GET", URL, true);
 xhr.onreadystatechange = () => {
-    // console.log(xhr.readyState);
-    // if (xhr.readyState === 4) {
-    //     const response = xhr.response;
-    //     // console.log(response);
-    //     const data = JSON.parse(response);
-    //     console.log(data);
-    // }
-    xhr.onload = () => {
-
-        const response = xhr.response;
-        // console.log(response);
-        const data = JSON.parse(response);
-        console.log(data);
-    }
-}
+  // console.log(xhr.readyState);
+  // if (xhr.readyState === 4) {
+  //     const response = xhr.response;
+  //     // console.log(response);
+  //     const data = JSON.parse(response);
+  //     console.log(data);
+  // }
+  xhr.onload = () => {
+    const response = xhr.response;
+    // console.log(response);
+    const data = JSON.parse(response);
+    console.log(data);
+  };
+};
 
 xhr.send();
