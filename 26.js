ab334b80ee197bcf75adf7fcd6c7a14ec84a381a
mahi5256 <m.p6092003@gmail.com>
2023-11-04 21:04:55 +0530

    json

--- 26.js
+++ 26.js
@@ -6,3 +6,7 @@ const person = {
 const extraInfo = new Map();
 extraInfo.set(person, { age: 15, phone: 15236 });
 console.log(extraInfo.get(person));
+console.log("script end");
+console.log('====================================');
+console.log();
+console.log('====================================');
