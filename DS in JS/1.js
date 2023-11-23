815838acd70976cfd185db895c5460a479c8a719
mahi5256 <m.p6092003@gmail.com>
2023-11-23 11:46:57 +0530

    setting the basic

--- DS in JS/1.js
+++ DS in JS/1.js
@@ -1,3 +1,9 @@
-console.log("====================================");
-console.log("hello world!");
-console.log("====================================");
+collection = [
+  ["a", 23, 22],
+  ["b", 3, 2],
+];
+console.log(collection);
+console.log(collection[1]);
+console.log(collection[0][0]);
+console.log(collection[0][1]);
+console.log(collection[0][2]);
