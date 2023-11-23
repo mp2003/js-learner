b24f6aa9302e5b9b777ffbffedf283ef69316857
Milind Pandey <56765253+mp2003@users.noreply.github.com>
2023-11-24 00:00:00 +0530

    Update 30.js

--- 30.js
+++ 30.js
@@ -1,4 +1,4 @@
-// call metho
+// call method
 const userMethod = {
     aboutRequest: function (hobby1, hobby2) {
       console.log(`name: ${this.name} age: ${this.age} email: ${this.email} hobby :`+hobby1 +`, `+hobby2 );
