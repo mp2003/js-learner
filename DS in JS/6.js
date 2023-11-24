fdabf1c8e9a9f6fd2704344855bc692541502b91
mahi5256 <m.p6092003@gmail.com>
2023-11-25 00:03:56 +0530

    priority queue implementation

--- DS in JS/6.js
+++ DS in JS/6.js
@@ -2,4 +2,47 @@
 
 function PriorityQueue() {
   var collection = [];
+
+  this.print = () => {
+    return collection;
+  };
+
+  this.size = () => {
+    return collection.length;
+  };
+
+  this.isEmpty = () => {
+    return collection.length === 0;
+  };
+
+  this.enQueue = (element) => {
+    if (this.isEmpty) collection.push(element);
+    else {
+      var added = false;
+      for (var i = 0; i < this.size(); i++) {
+        if (element[1] > collection[i][1]) {
+          collection.splice(i, 0, element);
+          added = true;
+          break;
+        }
+      }
+      if (!added) collection.push(element);
+    }
+  };
+  this.deQueue = () => {
+    return collection.shift();
+  };
+
+  this.front = () => {
+    return collection[0];
+  };
 }
+
+var MyQueue = new PriorityQueue();
+
+MyQueue.enQueue(["foo", 5]);
+MyQueue.enQueue(["bar", 1]);
+MyQueue.enQueue(["hell", 4]);
+MyQueue.enQueue(["bii", 3]);
+
+console.log(MyQueue.print());
