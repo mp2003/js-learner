889cf5f4b0fc80318e2c5ccdfe3a362945c3c12a
mahi5256 <m.p6092003@gmail.com>
2023-11-24 11:12:16 +0530

    palindrome

--- DS in JS/2.js
+++ DS in JS/2.js
@@ -1,3 +1,17 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// stack implementation
+
+function stack() {
+  var collection = [];
+  var word = prompt("enter a word");
+  var rword = "";
+  for (let i = 0; i < word.length; i++) collection.push(word[i]);
+  for (let i = 0; i < word.length; i++) {
+    rword += collection.pop();
+  }
+  console.log(word);
+  console.log(rword);
+  if (word === rword) console.log("palindrome");
+  else console.log("not a palindrome");
 }
+
+stack();
