c282b142285c73318ecc5d652a7fcb0814348b50
mp2003 <m.p6092003@gmail.com>
2024-01-26 20:59:48 +0530

    throttle

--- 54.js
+++ 54.js
@@ -4,7 +4,7 @@ const Debounce = document.querySelector(".debounce");
 const Throttle = document.querySelector(".throttle");
 
 // console.log(Input, Default, Debounce, Throttle);
-const debounce = (cb, delay) => {
+function debounce(cb, delay) {
   let timeout;
   return (...args) => {
     clearTimeout(timeout);
@@ -12,12 +12,41 @@ const debounce = (cb, delay) => {
       cb(...args);
     }, delay);
   };
-};
+}
+
 const updateDebounce = debounce((text) => {
   Debounce.textContent = text;
+}, 500);
+
+const updateThrottle = throttle((text) => {
+  Throttle.textContent = text;
 }, 1000);
 
 Input.addEventListener("input", (e) => {
   Default.textContent = e.target.value;
   updateDebounce(e.target.value);
+  updateThrottle(e.target.value);
 });
+
+function throttle(cb, delay) {
+  let shouldWait = false;
+  let waitingArgs;
+
+  const timeout = () => {
+    if (waitingArgs === null) shouldWait = false;
+    else {
+      cb(...waitingArgs);
+      setTimeout(timeout, delay);
+    }
+  };
+
+  return (...args) => {
+    if (shouldWait) {
+      waitingArgs = args;
+      return;
+    }
+    cb(...args);
+    shouldWait = true;
+    setTimeout(timeout, delay);
+  };
+}
