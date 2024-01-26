5bd36c6071c5077f23c391b670aeaba9e95c33e3
mp2003 <m.p6092003@gmail.com>
2024-01-26 20:52:45 +0530

    debounce

--- 54.js
+++ 54.js
@@ -3,4 +3,21 @@ const Default = document.querySelector(".default");
 const Debounce = document.querySelector(".debounce");
 const Throttle = document.querySelector(".throttle");
 
-console.log(Input, Default, Debounce, Throttle);
+// console.log(Input, Default, Debounce, Throttle);
+const debounce = (cb, delay) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+};
+const updateDebounce = debounce((text) => {
+  Debounce.textContent = text;
+}, 1000);
+
+Input.addEventListener("input", (e) => {
+  Default.textContent = e.target.value;
+  updateDebounce(e.target.value);
+});
